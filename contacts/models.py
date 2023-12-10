34318565d3fbe03075071de4e7f522ffc1f49eea
kushal.bh599@gmail.com <kushal.bh599@gmail.com>
2023-12-10 00:02:08 -0800

    Initial Commit

---
+++ contacts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+
+    def _str_(self):
+        return self.name 
